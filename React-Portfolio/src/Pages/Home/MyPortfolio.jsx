3216a05aa8d30cb4bb577979e6e5d05035527819
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-18 14:38:51 +0545

    Fix image rendering issue and update SVG attributes

--- React-Portfolio/src/Pages/Home/MyPortfolio.jsx
+++ React-Portfolio/src/Pages/Home/MyPortfolio.jsx
@@ -27,9 +27,9 @@ export default function MyPortfolio() {
       <div className="portfolio--section--container">
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
+            {/* <div className="portfolio--section--img">
               <img src={item.src} alt="Placeholder" />
-            </div>
+            </div> */}
             <div className="portfolio--section--card--content">
               <div>
                 <h3 className="portfolio--section--title">{item.title}</h3>
@@ -48,9 +48,9 @@ export default function MyPortfolio() {
                   <path
                     d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
                     stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2.66667"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               </p>
